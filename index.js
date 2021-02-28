45815736f97f634b6c2816753b2c20cdd6d36a64
Micah Banschick <micha.ban@gmail.com>
2021-02-28 20:23:23 +0000

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(time) {
+  if (parseInt(time) < 12) {
+    return 'Good Morning'
+  } else if (parseInt(time) >= 12 && parseInt(time) <= 17) {
+    return 'Good Afternoon'
+  } else {
+    return 'Good Evening'
+  }
+}
+
+function displayMessage(string) {
+  const greeting = document.getElementById('greeting');
+  greeting.innerText = string;
+}
